ead45a6dac7b4041fc94717473d2a610a6fcb73d
efJerryYang <efJerryYang@outlook.com>
2021-12-17 17:27:59 +0800

    Delete signal delay
    删除原仿真时误解的信号异常，而刻意做的时延。

--- LogicDesign/Experiment/Exp6/calculator_hex/calculator_hex.srcs/sources_1/imports/tmp_file/calculator_hex.v
+++ LogicDesign/Experiment/Exp6/calculator_hex/calculator_hex.srcs/sources_1/imports/tmp_file/calculator_hex.v
@@ -11,9 +11,9 @@ module calculator_hex (input wire clk,
     reg on_button          = 0;
     reg flag               = 0;
     reg [31:0] prev_result = 0;
-    reg [7:0] num1_delay   = 0;
-    reg [7:0] num2_delay   = 0;
-    reg [2:0] func_delay   = 0;
+    // reg [7:0] num1_delay   = 0;
+    // reg [7:0] num2_delay   = 0;
+    // reg [2:0] func_delay   = 0;
 
     always @(posedge clk, negedge rst_n) begin  // on_button, prev_result
         if (~rst_n || !locked)
@@ -23,11 +23,11 @@ module calculator_hex (input wire clk,
         else
             on_button <= on_button;
     end
-    always @(posedge clk) begin  // delay 1 clk period
-        num1_delay <= num1;
-        num2_delay <= num2;
-        func_delay <= func;
-    end
+    // always @(posedge clk) begin  // delay 1 clk period
+    //     num1_delay <= num1;
+    //     num2_delay <= num2;
+    //     func_delay <= func;
+    // end
     always @(posedge clk, negedge rst_n) begin
         if (~rst_n)
             flag <= 0;
@@ -41,7 +41,7 @@ module calculator_hex (input wire clk,
             prev_result <= 0;
         else if (!on_button)
             // prev_result <= (func == 3'd5) ? 0 : num1; // not on_button, prev square = 0, else prev = num1
-            prev_result <= num1;
+            prev_result <= num1;  // new requirement given during class
         else if (on_button && flag)
             prev_result <= cal_result;
         else
@@ -51,12 +51,12 @@ module calculator_hex (input wire clk,
         if (~rst_n)
             cal_result = 0;
         else if (button)
-            case(func_delay)
-                3'd0: cal_result    = prev_result + num2_delay; // add
-                3'd1: cal_result    = prev_result - num2_delay; // substract
-                3'd2: cal_result    = prev_result * num2_delay; // time
-                3'd3: cal_result    = prev_result / num2_delay; // divide
-                3'd4: cal_result    = prev_result % num2_delay; // mod
+            case(func)
+                3'd0: cal_result    = prev_result + num2; // add
+                3'd1: cal_result    = prev_result - num2; // substract
+                3'd2: cal_result    = prev_result * num2; // time
+                3'd3: cal_result    = prev_result / num2; // divide
+                3'd4: cal_result    = prev_result % num2; // mod
                 3'd5: cal_result    = prev_result * prev_result; // square
                 default: cal_result = 0;
             endcase
