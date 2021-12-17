f1ddc961dd1f6efe353e2032a5af40b281cd695a
efJerryYang <efJerryYang@outlook.com>
2021-12-17 17:47:43 +0800

    Update cnt logic of key_filter
    更新消抖计数器处理逻辑

--- LogicDesign/Experiment/Exp6/calculator_hex/calculator_hex.srcs/sources_1/new/key_filter.v
+++ LogicDesign/Experiment/Exp6/calculator_hex/calculator_hex.srcs/sources_1/new/key_filter.v
@@ -1,36 +1,51 @@
 `timescale 1ns / 1ps
 
-module key_filter(
-    input wire rst,
-    input wire clk,
-    input wire button,
-    output reg button_f
-    );
-    // parameter CNT_MAX = 32'd500_0000;
-    parameter CNT_MAX = 32'd3;  // simulation
+module key_filter(input wire rst,
+                  input wire clk,
+                  input wire button,
+                  output reg button_f);
+    // parameter CNT_MAX = 32'd1000;
+    parameter CNT_MAX    = 32'd3;  // simulation
     
-    wire rst_n = ~rst;
+    wire rst_n     = ~rst;
     reg [31:0] cnt = 0;
-
+    
     always @(*) begin
-        if(~rst_n)
+        if (~rst_n)
             button_f = 0;
         else if (cnt == CNT_MAX && button)
             button_f = 1;
-        else 
+        else
             button_f = 0;
     end
+    // always @(posedge clk, negedge rst_n) begin
+    //     if (~rst_n)
+    //         cnt <= 0;
+    //     else if (cnt < CNT_MAX)
+    //         cnt <= cnt + 1;
+    //     else if (cnt == CNT_MAX)
+    //         if (button)
+    //             cnt <= 0;
+    //         else
+    //             cnt <= cnt;
+    //     else
+    //         cnt <= cnt;
+    // end
     always @(posedge clk, negedge rst_n) begin
-        if(~rst_n)
+        if (~rst_n)
             cnt <= 0;
-        else if(cnt < CNT_MAX)
-            cnt <= cnt + 1;
-        else if (cnt == CNT_MAX)
-            if (button)
+        else if (button) begin
+            if (cnt == CNT_MAX)
                 cnt <= 0;
-            else 
+            else
                 cnt <= cnt;
-        else
-            cnt <= cnt;
+        end
+        else  begin
+            if (cnt == CNT_MAX)
+                cnt <= cnt;
+            else
+                cnt <= cnt + 1;
+        end
+        
     end
 endmodule
