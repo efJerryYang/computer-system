63f4ed4c0d26db17fc56afc0d6e6136ff2d28550
Jerry Yang <efJerryYang@outlook.com>
2022-04-20 22:13:42 +0800

    feat: lab1 code update

--- comp-organ/lab1/src/main.c
+++ comp-organ/lab1/src/main.c
@@ -1,10 +1,23 @@
 #include "multiply.h"
+#include <stdio.h>
+uint16_t multiply(uint16_t x, uint16_t y)
+{
+    // cast uint8_t to uint16_t to store 16-bit result
+    uint16_t result = 0;
+    while (y != 0)
+    {
+        result += (y & 1) ? x : 0;
+        y >>= 1;
+        x <<= 1;
+    }
+    return result;
+}
 
 int main()
 {
     uint8_t x = 28;
-    uint8_t y = multiply(x, x);
-    y = multiply(y,x);
-    printf("%u\n", y);
+    uint16_t y = multiply(x, x);
+    uint16_t result = multiply(y, x);
+    printf("%u\n", result);
     return 0;
 }
