f50c0e72558fa537983643c033e3e81fbfb0b133
Jerry Yang <efJerryYang@outlook.com>
2022-04-21 12:37:55 +0800

    feat: update lab-submit

---
+++ comp-organ/lab1/lab-submit/src/multiply.h
@@ -0,0 +1,9 @@
+#ifndef _MY_MULTIPLY_H_
+#define _MY_MULTIPLY_H_
+
+typedef __UINT8_TYPE__ uint8_t;
+typedef __UINT16_TYPE__ uint16_t;
+
+int printf (const char *__format, ...); // declaration of printf()
+
+#endif
