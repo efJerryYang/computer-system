75bd5f5efe09cfa5a564d918f242f2ddeda54059
Jerry Yang <efJerryYang@outlook.com>
2022-05-08 15:43:10 +0800

    chore: add comments for submit

--- comp-organ/lab3/booth/booth.srcs/sources_1/imports/booth/booth.v
+++ comp-organ/lab3/booth/booth.srcs/sources_1/imports/booth/booth.v
@@ -13,21 +13,25 @@ parameter CNT_MAX = PERIOD_NUM-1;
 reg [15:0] x_reg;
 reg [15:0] y_reg;
 reg [15:0] nx_reg;
-reg [1:0] comparator;                        // y_n, y_n+1
+reg [1:0] comparator; // yn, yn+1 but in reversed position [ yn+1 | yn ]
 reg [4:0] cnt;
 reg busy_reg;
-reg on_button;
+reg on_button;        // a flag for start working
 
-wire [16:0] z_part_mul =
-        comparator[1] == comparator[0] ? z[31:15]:                             // yn+1 == yn
-        comparator[1] > comparator[0] ? {x_reg[15], x_reg} + z[31:15] :     // yn+1 > yn
-        {nx_reg[15], nx_reg} + z[31:15];                                    // yn+1 < yn
-wire [1:0] comparator_new = {comparator[0], (z[0] & 1'b1)};
+// compute partial product (z_pp) of z
+wire [16:0] z_pp =
+   comparator[1] == comparator[0] ? z[31:15]:                          // yn+1 == yn
+   comparator[1] > comparator[0] ? {x_reg[15], x_reg} + z[31:15] :     // yn+1 > yn
+   {nx_reg[15], nx_reg} + z[31:15];                                    // yn+1 < yn
 
+// compute comparator for next step
+wire [1:0] comparator_new = {comparator[0], z[0]};
+// wire [1:0] check_zsign = z[31:30];
+
+// assign given wire with reg
 assign busy = busy_reg;
 
-// wire [1:0] check_zsign = z[31:30];
-always @(posedge clk, negedge rst_n) begin // on_button
+always @(posedge clk, negedge rst_n) begin // on_button, a flag for start working
     if (~rst_n)
         on_button <= 0;
     else if(start)
@@ -36,7 +40,7 @@ always @(posedge clk, negedge rst_n) begin // on_button
         on_button <= on_button;
 end
 
-always @(posedge clk, negedge rst_n) begin // get input signal for x, y and negative x
+always @(posedge clk, negedge rst_n) begin // x, y and negative x (nx_reg)
     if(~rst_n) begin
         x_reg <= 0;
         y_reg <= 0;
@@ -54,7 +58,7 @@ always @(posedge clk, negedge rst_n) begin // get input signal for x, y and nega
     end
 end
 
-always @(posedge clk, negedge rst_n) begin // handle busy signal
+always @(posedge clk, negedge rst_n) begin // busy
     if (~rst_n)
         busy_reg <= 0;
     else if(start || (on_button && cnt < CNT_MAX))
@@ -63,33 +67,34 @@ always @(posedge clk, negedge rst_n) begin // handle busy signal
         busy_reg <= 0;
 end
 
-always @(posedge clk, negedge rst_n) begin // handle z and comparator
-// z index: [ 31 | 30 ... 15 | 14 ... 0 ], 31 and 30 should be regarded as operands
-// z index: [ 31  30 ... 15 | 14 ... 0 ], and 1-bit of sign should be appended to the most significant bit of x (or negative x)
+// z index: [ 31 | 30 ... 15 | 14 ... 0 ]
+// sign bits: 31, 30
+// 1-bit of sign should be appended to the most significant bit of x (or negative x)
+always @(posedge clk, negedge rst_n) begin // z and comparator
     if(~rst_n) begin
         z <= 0;
         comparator <= 0;
     end
     else if(start) begin
-        z <= (y >> 1);                    // >>> shift right arithemtical, >> shift right logical
-        comparator <= (y & 1'b1);       // index: [ 1 | 0 ] --- [ yn+1 | yn ]
+        z <= (y >> 1);
+        comparator <= {1'b0, y[0]};             // index: [ 1 | 0 ] --- [ yn+1 | yn ]
     end
-    else if (on_button && cnt < CNT_MAX) begin                                // booth algorithm, 15 time operations in total
-        z <= {z_part_mul[16], z_part_mul, z[14:1]};                         // signed(z) shift right arithmetical
+    else if (on_button && cnt < CNT_MAX) begin  // booth algorithm, 15 time operations in total
+        z <= {z_pp[16], z_pp, z[14:1]};         // z_pp: do sign extension, z: discard right most 1 bit
         comparator <= comparator_new;
     end
     else if (on_button && cnt == CNT_MAX && busy) begin  // finish
-        z <= {z_part_mul, z[14:0]};			// do not shift right
-        comparator <= comparator_new;  
+        z <= {z_pp, z[14:0]};                            // do not shift right
+        comparator <= comparator_new;
     end
-    else begin // do nothing
+    else begin                                  // do nothing
         z <= z;
         comparator <= comparator;
     end
 end
 
 always @(posedge clk, negedge rst_n) begin
-    if(~rst_n||~on_button)
+    if(~rst_n || ~on_button)
         cnt <= 0;
     else if (start)
         cnt <= 0; 
--- comp-organ/lab3/booth2/booth2.srcs/sources_1/imports/booth2/booth2.v
+++ comp-organ/lab3/booth2/booth2.srcs/sources_1/imports/booth2/booth2.v
@@ -7,35 +7,38 @@ module booth2 (
     output wire [31:0] z    ,
     output wire        busy 
 );
+
 parameter PERIOD_NUM = 8;
 parameter CNT_MAX = PERIOD_NUM-1;
 
 reg [15:0] x_reg;
 reg [15:0] y_reg;
 reg [15:0] nx_reg;
-reg [15:0] dx_reg; // double x
-reg [15:0] ndx_reg; // negative double x
 reg [31:0] z_reg;
-reg [2:0] comparator;                        // y_n, y_n+1
+reg [2:0] comparator; // yn, yn+1, yn+2 but in reversed position [ yn+2 | yn+1 | yn ]
 reg [4:0] cnt;
 reg busy_reg;
-reg on_button;
+reg on_button;        // a flag for start working
 
-wire [16:0] z_part_mul = 
-            comparator == 3'b000 ? z[30:14] : 
-            comparator == 3'b100 ? {x_reg[15], x_reg} + z[30:14]: 
-            comparator == 3'b010 ? {x_reg[15], x_reg} + z[30:14]:
-            comparator == 3'b110 ? {x_reg[15], x_reg} + {x_reg[15], x_reg} + z[30:14]:
-            comparator == 3'b001 ? {nx_reg[15], nx_reg} + {nx_reg[15], nx_reg} + z[30:14]:
-            comparator == 3'b101 ? {nx_reg[15], nx_reg} + z[30:14]:
-            comparator == 3'b011 ? {nx_reg[15], nx_reg} + z[30:14]:z[30:14];
+// compute partial product (z_pp) of z
+wire [16:0] z_pp = 
+	  comparator == 3'b000 ? z[30:14] : 
+	  comparator == 3'b100 ? {x_reg[15], x_reg} + z[30:14] : 
+	  comparator == 3'b010 ? {x_reg[15], x_reg} + z[30:14] :
+	  comparator == 3'b110 ? {x_reg[15], x_reg} + {x_reg[15], x_reg} + z[30:14] :
+	  comparator == 3'b001 ? {nx_reg[15], nx_reg} + {nx_reg[15], nx_reg} + z[30:14] :
+	  comparator == 3'b101 ? {nx_reg[15], nx_reg} + z[30:14] :
+	  comparator == 3'b011 ? {nx_reg[15], nx_reg} + z[30:14] : z[30:14];
+
+// compute comparator for next step
 wire [2:0] comparator_new = {comparator[0], z[0], z[1]};
+// wire [1:0] check_zsign = z[31:30];
 
+//assign given wire with reg
 assign busy = busy_reg;
 assign z = z_reg;
 
-// wire [1:0] check_zsign = z[31:30];
-always @(posedge clk, negedge rst_n) begin // on_button
+always @(posedge clk, negedge rst_n) begin // on_button, a flag for start working
     if (~rst_n)
         on_button <= 0;
     else if(start)
@@ -44,31 +47,25 @@ always @(posedge clk, negedge rst_n) begin // on_button
         on_button <= on_button;
 end
 
-always @(posedge clk, negedge rst_n) begin // x, y and nx, dx, ndx
+always @(posedge clk, negedge rst_n) begin // x, y and nx
     if(~rst_n) begin
         x_reg <= 0;
         y_reg <= 0;
         nx_reg <= 0;
-		dx_reg <= 0;
-		ndx_reg <= 0;
     end
     else if(start) begin
         x_reg <= x;
         y_reg <= y;
         nx_reg <= ~x+1;
-		dx_reg <= x<<1;
-		ndx_reg <= ~(x<<1)+1;
     end
     else begin
         x_reg <= x_reg;
         y_reg <= y_reg;
         nx_reg <= nx_reg;
-		dx_reg <= dx_reg;
-		ndx_reg <= ndx_reg;
     end
 end
 
-always @(posedge clk, negedge rst_n) begin // handle busy signal
+always @(posedge clk, negedge rst_n) begin // busy
     if (~rst_n)
         busy_reg <= 0;
     else if(start || (on_button && cnt < CNT_MAX))
@@ -77,33 +74,34 @@ always @(posedge clk, negedge rst_n) begin // handle busy signal
         busy_reg <= 0;
 end
 
-always @(posedge clk, negedge rst_n) begin // handle z and comparator
-// z index: [ 31 | 30 ... 15 | 14 ... 0 ], 31 and 30 should be regarded as operands
-// z index: [ 31  30 ... 15 | 14 ... 0 ], and 1-bit of sign should be appended to the most significant bit of x (or negative x)
+// z index: [ 31 | 30 29 ... 15  14 [ 13 ... 0 ]. 
+// sign bits: 30, 29, the left most bit is not used
+// 1-bit of sign should be appended to the most significant bit of x (or negative x)
+always @(posedge clk, negedge rst_n) begin // z and comparator
     if(~rst_n) begin
         z_reg <= 0;
         comparator <= 0;
     end
     else if(start) begin
-        z_reg <= (y >> 2);                    // >>> shift right arithemtical, >> shift right logical
-        comparator <= {1'b0, y[0], y[1]};       // index: [ 1 | 0 ] --- [ yn+1 | yn ]
+        z_reg <= (y >> 2);
+        comparator <= {1'b0, y[0], y[1]};                // index: [ 2 | 1 | 0 ] --> [ yn+2 | yn+1 | yn ]
     end
-    else if (on_button && cnt < CNT_MAX) begin                                // booth algorithm, 7 time operations in total
-        z_reg <= {z_part_mul[16], z_part_mul[16], z_part_mul, z[13:2]};                         // signed(z) shift right arithmetical
+    else if (on_button && cnt < CNT_MAX) begin           // booth algorithm enhanced, 7 time operations in total
+        z_reg <= {z_pp[16], z_pp[16], z_pp, z[13:2]};    // z_pp: do sign extension, z: discard right most 2 bits
         comparator <= comparator_new;
     end
     else if (on_button && cnt == CNT_MAX && busy) begin  // finish
-		z_reg <= {z_part_mul[16], z_part_mul, z[13:0]}; // do not shift right
+        z_reg <= {z_pp[16], z_pp, z[13:0]};              // do not shift right
         comparator <= comparator_new;
     end
-    else begin // do nothing
+    else begin                                           // do nothing
         z_reg <= z;
         comparator <= comparator;
     end
 end
 
-always @(posedge clk, negedge rst_n) begin
-    if(~rst_n||~on_button)
+always @(posedge clk, negedge rst_n) begin  // cnt: 0, 1, 2, ..., CNT_MAX
+    if(~rst_n || ~on_button)
         cnt <= 0;
     else if (start)
         cnt <= 0; 
