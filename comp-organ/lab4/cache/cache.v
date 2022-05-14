47b640f48f66831edb047f78956af352c945c28e
Jerry Yang <efJerryYang@outlook.com>
2022-05-14 19:13:53 +0800

    feat: lab4 test pass (fix previous omission)
    feat: lab4 test pass (fix previous omission)
    
    chore:

--- comp-organ/lab4/cache/cache.v
+++ comp-organ/lab4/cache/cache.v
@@ -24,21 +24,23 @@ module cache (
 );
 
 reg [3:0] current_state, next_state;
-localparam READY     = 4'b0000,
-           TAG_CHECK = 4'b0010,
-           REFILL    = 4'b0001;
+localparam READY           = 4'b0000,
+           TAG_CHECK_READ  = 4'b0010,
+           TAG_CHECK_WRITE = 4'b0100,
+           REFILL          = 4'b0001,
+           WR_DATA         = 4'b0011;
 
-wire        wea;                                // Cache写使能信号
-wire [37:0] cache_line_r   = {1'b1, addr_from_cpu[12:8], rdata_from_mem}; /* TODO */         // 待写入Cache的Cache行数据
-wire [37:0] cache_line;                         // 从Cache中读出的Cache行数据
+wire        wea;                                  // Cache写使能信号
+wire [37:0] cache_line_r;                         // 待写入Cache的Cache行数据
+wire [37:0] cache_line;                           // 从Cache中读出的Cache行数据
 
-wire [ 5:0] cache_index    = addr_from_cpu[7:2];  /* TODO */         // 主存地址中的Cache索引/Cache地址
-wire [ 4:0] tag_from_cpu   = addr_from_cpu[12:8]; /* TODO */         // 主存地址的Tag
-wire [ 1:0] offset         = addr_from_cpu[1:0];  /* TODO */         // Cache行内的字节偏移
-wire        valid_bit      = cache_line[37];      /* TODO */         // Cache行的有效位
-wire [ 4:0] tag_from_cache = cache_line[36:32];   /* TODO */         // Cache行的Tag
+wire [ 5:0] cache_index    = addr_from_cpu[7:2];  // 主存地址中的Cache索引/Cache地址
+wire [ 4:0] tag_from_cpu   = addr_from_cpu[12:8]; // 主存地址的Tag
+wire [ 1:0] offset         = addr_from_cpu[1:0];  // Cache行内的字节偏移
+wire        valid_bit      = cache_line[37];      // Cache行的有效位
+wire [ 4:0] tag_from_cache = cache_line[36:32];   // Cache行的Tag
 
-wire hit  = (tag_from_cache==tag_from_cpu) && valid_bit && (current_state==TAG_CHECK);  /* Todo */
+wire hit  = (tag_from_cache == tag_from_cpu) && valid_bit && (current_state == TAG_CHECK_READ || current_state == TAG_CHECK_WRITE);
 wire miss = (tag_from_cache != tag_from_cpu) | (~valid_bit);
 
 // 根据Cache行的字节偏移，从Cache块中选取CPU所需的字节数据
@@ -57,7 +59,6 @@ blk_mem_gen_0 u_cache (
     .douta  (cache_line  )
 );
 
-
 always @(posedge clk) begin
     if (reset) begin
         current_state <= READY;
@@ -70,24 +71,40 @@ end
 always @(*) begin
     case(current_state)
         READY: begin
-            if (rreq_from_cpu) begin        /* Todo */
-                next_state = TAG_CHECK;     /* Todo */
+            if (rreq_from_cpu) begin
+                next_state = TAG_CHECK_READ;
+            end else if(wreq_from_cpu) begin
+                next_state = TAG_CHECK_WRITE;
             end else begin
-                next_state = READY;         /* Todo */
+                next_state = READY; 
             end
         end
-        TAG_CHECK: begin
-            if (miss) begin  // miss        /* Todo */
-                next_state = REFILL;        /* Todo */
+        TAG_CHECK_READ: begin
+            if (miss && rreq_from_cpu) begin
+                next_state = REFILL;
             end else begin
-                next_state = READY;         /* Todo */
+                next_state = READY; 
+            end
+        end
+        TAG_CHECK_WRITE: begin
+            if (hit) begin
+                next_state = WR_DATA;
+            end else begin
+                next_state = READY; 
             end
         end
         REFILL: begin
-            if (rvalid_from_mem) begin      /* Todo */
-                next_state = TAG_CHECK;     /* Todo */
+            if (rvalid_from_mem) begin 
+                next_state = TAG_CHECK_READ;
             end else begin 
-                next_state = REFILL;        /* Todo */
+                next_state = REFILL;
+            end
+        end
+        WR_DATA: begin
+            if (wea) begin
+                next_state = TAG_CHECK_WRITE;
+            end else begin
+                next_state = WR_DATA;
             end
         end
         default: begin
@@ -97,26 +114,26 @@ always @(*) begin
 end
 
 // 生成Block RAM的写使能信号
-assign wea = (current_state == REFILL) && rreq_from_cpu; //! ? tag check | rreq_from_cpu  /* Todo */
+assign wea = (current_state == REFILL && rvalid_from_mem) || current_state == WR_DATA; 
 
 // 生成读取主存所需的信号，即读请求信号rreq_to_mem和读地址信号raddr_to_mem
 always @(posedge clk) begin
     if (reset) begin
         raddr_to_mem <= 0;
-        rreq_to_mem <= 0;
+        rreq_to_mem  <= 0;
     end else begin
         case (next_state)
             READY: begin
-                raddr_to_mem <=  0;              /* TODO */
-                rreq_to_mem  <=  0;              /* TODO */
+                raddr_to_mem <= 0;
+                rreq_to_mem  <= 0;
             end
-            TAG_CHECK: begin
-                raddr_to_mem <=  0;              /* TODO */
-                rreq_to_mem  <=  0;              /* TODO */
+            TAG_CHECK_READ: begin
+                raddr_to_mem <= 0;
+                rreq_to_mem  <= 0;
             end
             REFILL: begin
-                raddr_to_mem <=  addr_from_cpu;   /* TODO */
-                rreq_to_mem  <=  rreq_from_cpu;   /* TODO */
+                raddr_to_mem <= addr_from_cpu;
+                rreq_to_mem  <= rreq_from_cpu;
             end
             default: begin
                 raddr_to_mem <= 0;
@@ -127,10 +144,16 @@ always @(posedge clk) begin
 end
 
 // 写命中处理（写直达法）：写命中时，既要更新Cache块，也要更新内存数据
-/* TODO */
+wire [31:0] wdata_to_cache = offset == 2'b00 ? {1'b1, tag_from_cpu, cache_line[31: 8], wdata_from_cpu} :
+                             offset == 2'b01 ? {1'b1, tag_from_cpu, cache_line[31:16], wdata_from_cpu, cache_line[ 7:0]} :
+                             offset == 2'b10 ? {1'b1, tag_from_cpu, cache_line[31:24], wdata_from_cpu, cache_line[15:0]} :
+                             offset == 2'b11 ? {1'b1, tag_from_cpu, wdata_from_cpu, cache_line[23:0]} :
+                             rdata_from_mem;
+assign cache_line_r = wreq_from_cpu ? {1'b1, tag_from_cpu, wdata_to_cache} : {1'b1, tag_from_cpu, rdata_from_mem};
+
 always @(posedge clk) begin
     if (reset) begin
-        wreq_to_mem <= 0;
+        wreq_to_mem  <= 0;
         waddr_to_mem <= 0;
         wdata_to_mem <= 0;
     end else begin
@@ -140,12 +163,12 @@ always @(posedge clk) begin
                 waddr_to_mem <= 0;
                 wdata_to_mem <= 0;
             end
-            TAG_CHECK: begin
+            TAG_CHECK_WRITE: begin
                 wreq_to_mem  <= 0;
                 waddr_to_mem <= 0;
                 wdata_to_mem <= 0;
             end
-            REFILL: begin
+            WR_DATA: begin
                 wreq_to_mem  <= wreq_from_cpu;
                 waddr_to_mem <= addr_from_cpu;
                 wdata_to_mem <= wdata_from_cpu;
